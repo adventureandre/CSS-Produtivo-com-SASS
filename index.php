48e1a77fc5df4b2fb0f8be0c9456fa4e36b1f19a
Andre Luiz <adventureandre@hotmail.com.br>
2022-05-18 21:23:38 -0300

    CSS Produtivo com SASS

---
+++ index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="_cdn/css/style.css">
+</head>
+<body>
+<div class="main_header">
+    <nav class="main_header_navigation">
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Portifolio</a></li>
+            <li><a href="">Quem Somos</a></li>
+            <li><a href="">Contato</a></li>
+        </ul>
+    </nav>
+</div>
+<div class="main_site">
+    <div class="main_site_content">
+        <h1>Teste de conteúdo</h1>
+    </div>
+    <button class="btn btn-green btn-large">Clique aqui!</button>
+</div>
+
+<div class="main_footer">
+    <div class="main_footer_content">
+        <p>Todos os Direitos são reservados!</p>
+    </div>
+</div>
+</body>
+</html>
